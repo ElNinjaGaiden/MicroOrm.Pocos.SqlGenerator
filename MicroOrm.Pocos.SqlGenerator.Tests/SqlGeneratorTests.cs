2a5b6637663e9b31a671033fcfb24cf25de558ac
Shawson <shawson@gmail.com>
2016-04-02 17:23:25 +0100

    Fixed issue when selecting records based on a column having a null value
    You can now select records based on a null column value.  Added tests to
    support this and ensure existing select functionality wasn't knocked
    out.

---
+++ MicroOrm.Pocos.SqlGenerator.Tests/SqlGeneratorTests.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MicroOrm.Pocos.SqlGenerator.Attributes;
+
+namespace MicroOrm.Pocos.SqlGenerator.Tests
+{
+    [TestClass]
+    public class SqlGeneratorTests
+    {
+        [TestMethod]
+        public void GetWhere_TestForNulls()
+        {
+            var sqlGenerator = new SqlGenerator<MyObject>();
+
+            var sql = sqlGenerator.GetSelect(new { Disabled = (DateTime?)null });
+
+            Assert.IsTrue(sql.Contains("WHERE [MyTable].[Disabled] IS NULL"), sql);
+        }
+
+        [TestMethod]
+        public void GetWhere_TestForValueInNullableColumn()
+        {
+            var sqlGenerator = new SqlGenerator<MyObject>();
+
+            var sql = sqlGenerator.GetSelect(new { Disabled = new DateTime(2016,02,04,0,0,0) });
+
+            Assert.IsTrue(sql.Contains("WHERE [MyTable].[Disabled] = @Disabled"), sql);
+        }
+    }
+
+    [StoredAs("MyTable")]
+    public class MyObject
+    {
+        [KeyProperty]
+        public int MyObjectId { get; set; }
+
+        public string Description { get; set; }
+
+        public DateTime? Disabled { get; set; }
+    }
+}
